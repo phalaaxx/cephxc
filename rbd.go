f0c4ed1ae4b00c7e5b05fd9f7eb4a0a8e0ec13c4
Bozhin Zafirov <bozhin@sirius.local>
2017-08-04 15:26:52 +0300

    combine rbd and rbdlist modules

--- rbd.go
+++ rbd.go
@@ -3,11 +3,59 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 /* devpath specifies path to devices interface for kernel rbd module */
 const devpath = "/sys/bus/rbd/devices"
+
+/* RbdList represents list of ceph rbd devices */
+type RbdList []int
+
+/* Exists returns true if n is present in RbdList, false otherwise */
+func (r RbdList) Exists(n int) bool {
+	for _, i := range r {
+		if i == n {
+			return true
+		}
+	}
+	return false
+}
+
+/* GetNew finds and returns first outersection item in RbdList if it exists, -1 otherwise */
+func (r RbdList) GetNew(n RbdList) int {
+	for _, i := range n {
+		if !r.Exists(i) {
+			return i
+		}
+	}
+	return -1
+}
+
+/* NewRbdList returns list of currently mapped rbd devices */
+func NewRbdList(path string) (result RbdList) {
+	// open devices list for reading
+	d, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	// read list of devices
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return
+	}
+	// parse names
+	for _, idStr := range names {
+		// parse integer
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return
+		}
+		result = append(result, id)
+	}
+	return
+}
 
 /* RbdMap instructs kernel rbd module to map a ceph device */
 func RbdMap(pool, name string) (int, error) {
